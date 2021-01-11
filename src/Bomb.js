b0ac33096f5e423c764051f556ba9e79ab3135cb
Muhidin <muhidinhukic1@gmail.com>
2021-01-11 11:10:21 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,32 @@
-// your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  clockBomb = () => {
+    const newTime = this.state.secondsLeft - 1;
+    this.setState({
+      secondsLeft: newTime,
+    });
+
+    console.log(this.state.secondsLeft);
+  };
+
+  render() {
+    return (
+      <div className="bomb-timer" onClick={this.clockBomb}>
+        <p>
+          {this.state.secondsLeft === 0
+            ? "Boom!"
+            : `${this.state.secondsLeft} seconds left before I go boom!`}
+        </p>
+      </div>
+    );
+  }
+}
